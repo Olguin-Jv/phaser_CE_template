d9aa3588ce172072d2322966e2ee2dc236b50287
OlguinJv <olguin.j.vito@gmail.com>
2018-12-31 14:05:59 -0300

    fast commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 
 if (!window.orientation) {
-	gameX = 800;
+    gameX = 800;
     gameY = 600;
 }
 if (window.orientation == 0 || window.orientation == 90 || window.orientation == -90) {
-	gameX = 400;
+    gameX = 400;
     gamey = 500;
-	showControls = true;
+    showControls = true;
+    showRotate();
 }
+
+function showRotate(){
+    var rotate = document.getElementById('rotate');
+        rotate.classList.add('rotate');
+    var p = document.createElement('p');
+        p.innerHTML = 'Por favor rota el dispositivo';
+    rotate.appendChild(p);
+};
+
 
 var game = new Phaser.Game(gameX, gameY, Phaser.AUTO);
 game.parent = 'game-area';
